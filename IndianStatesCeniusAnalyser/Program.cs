51ed801ea592c83e6d5a6015b3cae34aa2be2d5b
vundelayamini <vundelayamini@gmail.com>
2021-05-12 22:42:47 +0530

    [yamini]Add: Welcome message to indian states census analyser

---
+++ IndianStatesCeniusAnalyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStatesCeniusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Indian States Census Analyser program");
+        }
+    }
+}
